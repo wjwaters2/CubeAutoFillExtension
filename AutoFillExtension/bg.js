e2298c10087db2ed018f7895ba99f59188fb3df6
wjwaters2 <wjwaters2@gaig.com>
2017-03-10 11:14:18 -0500

    First commit. CUBE subInfo autofill extention

---
+++ AutoFillExtension/bg.js
@@ -0,0 +1,9 @@
+var id = "gbcggmopjpddhkgdabjhfhhjckgopihp";
+function reloadExtension(id) {
+    chrome.management.setEnabled(id, false, function() {
+        chrome.management.setEnabled(id, true);
+    });
+}
+chrome.browserAction.onClicked.addListener(function(tab) {
+    reloadExtension(id);
+});
